549779fa442a17e73548b70467f317798b5ed378
anandjayan <anandudjayan29397@gmail.com>
2023-09-19 20:20:17 +0530

    toggler issue fixed

--- rooms.php
+++ rooms.php
@@ -1,72 +1,60 @@
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>AquaZen Houseboats- Rooms</title>
-        
-        
-        <?php require('include/links.php') ?>
-        <!-- Font Awesome -->
-        <link
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
-        
-      
-    </head>
-      <body>
-             <?php require('include/header.php') ?>
-             
-                    <div class="my-5 px-4">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>AquaZen Houseboats- rooms</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <?php require('include/links.php') ?>
+  </head>
+  <body >
+  <?php require('include/header.php') ?>
+
+  <div class="my-5 px-4">
                         <h2 class="fw-bold h-font text-center">Our Rooms</h2>
                             <div class="h-line bg-dark">
                             </div>
                     </div>
 
              <div class="container">
-                <div class="row">
-                  <div class="col-lg-3">
-                  <nav class="navbar navbar-expand-lg bg-body-tertiary  text-dark">
-                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Navbar</a>
-                        <button class="navbar-toggler bg-dark text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                           <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                           <ul class="navbar-nav">
-                           <li class="nav-item">
-                              <a class="nav-link active" aria-current="page" href="#">Home</a>
-                           </li>
-                           <li class="nav-item">
-                              <a class="nav-link" href="#">Features</a>
-                           </li>
-                           <li class="nav-item">
-                              <a class="nav-link" href="#">Pricing</a>
-                           </li>
-                           <li class="nav-item dropdown">
-                              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                 Dropdown link
-                              </a>
-                              <ul class="dropdown-menu">
-                                 <li><a class="dropdown-item" href="#">Action</a></li>
-                                 <li><a class="dropdown-item" href="#">Another action</a></li>
-                                 <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  <div class="row">
+                        <div class="col-lg-3">
+                        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                           <div class="container-fluid">
+                           <a class="navbar-brand" href="#">Navbar</a>
+                           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                              <span class="navbar-toggler-icon"></span>
+                           </button>
+                           <div class="collapse navbar-collapse" id="navbarNav">
+                              <ul class="navbar-nav">
+                                 <li class="nav-item">
+                                 <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                 </li>
+                                 <li class="nav-item">
+                                 <a class="nav-link" href="#">Features</a>
+                                 </li>
+                                 <li class="nav-item">
+                                 <a class="nav-link" href="#">Pricing</a>
+                                 </li>
+                                 <li class="nav-item">
+                                 <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                                 </li>
                               </ul>
-                           </li>
-                           </ul>
-                        </div>
-                       </div>
-                     </nav>
-                  </div>  
+                           </div>
+                           </div>
+                        </nav>
+                        </div>  
 
                     
-                </div>
+                  </div>
              </div>       
-       
 
-             <?php require('include/footer.php') ?>
-            
-        </body>
+    
+    
+    
+    
+    <?php require('include/footer.php') ?> 
+  </body>
 </html>
-
-<!-- start 5 video, 4 mins, change the social media icons to bootstrap icons because its messing up with bootstrap codes -->
